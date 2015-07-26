bd23f06a1faea147e3104e75af1c402e50c23746
vagrant <vagrant@enpit>
2015-07-26 14:25:43 +0000

    add mul

--- test/mini_culculator_test.rb
+++ test/mini_culculator_test.rb
@@ -17,10 +17,12 @@ class MiniCulculattor < Minitest::Test
 
     def test_mul
         assert @calc.mul(0, 0) == 0, "mul(0, 0) failed"
+        assert @calc.mul(2, 2) == 4, "mul(2, 2) failed"
     end
 
     def test_div
         assert @calc.div(1, 1) == 1, "div(1, 1) failed"
+        assert @calc.div(4, 2) == 1, "div(4, 2) failed"
     end
 
 
